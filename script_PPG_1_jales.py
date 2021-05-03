805d9644c13a8eb4a6973fd6a8a7df4d9262f66d
Jales Bussinguer <63973509+JalesBussinguer@users.noreply.github.com>
2021-05-03 17:22:57 -0300

    updates

--- script_PPG_1_jales.py
+++ script_PPG_1_jales.py
@@ -5,21 +5,78 @@ Created on Fri Apr 30 12:00:24 2021
 @author: jales
 """
 
-# Programa que calcula a cota em pontos intermediários a outros com cotas previamente medidas
+# Programa que calcula a cota em pontos intermediários a partir de um levantamento topográfico previamente realizado
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import OrderedDict
+
+# Pergunta
+
+arquivo_ref=str(input('Informe o arquivo de referência: ', ))
+
+
+arquivo_esp=str(input('Informe os dados dos espaçamentos: ', ))
+print()
 
 # Leitura do arquivo contendo os dados de entrada
 
-medicoes = pd.read_csv('dados_cotas.csv', sep=',')
+referencia = pd.read_csv(arquivo_ref, sep=',')
 
-marco = medicoes['marco']
-cota = medicoes['cota']
-distancia = medicoes['distancia']
+cota = referencia['cota']
+distancia = referencia['distancia']
 
-# Plotagem inicial dos dados (visualização primária)
+print(referencia)
+print()
+
+# Criação da lista com os valores de espaçamentos para a interpolação
+
+medicoes = pd.read_csv(arquivo_esp, sep=',')
+
+lista_esp = medicoes['espacamento'].tolist()
+print()
+
+print(f'espaçamentos para interpolação [m] = {(sorted(lista_esp))}')
+print()
+
+# Criação de um dicionário das coordenadas
+
+dict_coords = {}
+
+# Cálculo das cotas por interpolação linear do tipo y = ax + b
+
+for i in lista_esp:
+    if 0 < i < 100:
+        y = 3.82 * i + 382
+    elif 101 < i < 200:
+        y = 0.3 * i + 736
+    elif 201 < i < 300:
+        y = -6.09 * i + 2014
+    elif 301 < i < 400:
+        y = 3.03 * i - 722
+    elif 401 < i < 500:
+        y = -0.44 * i + 666
+    elif 501 < i < 600:
+        y = 2.01 * i - 559
+    elif 601 < i < 700:
+        y = 0.63 * i + 269
+    elif 701 < i < 800:
+        y = 0.45 * i + 395
+    elif 801 < i < 900:
+        y = -4.78 * i + 4579
+    elif 901 < i < 1000:
+        y = 2.03 * i - 1550
+    else:
+        print('dados fora do domínio')
+        
+# População do dicionário com as coordenadas
+    dict_coords.update({i:y})
+
+# Organização do dicionário em ordem crescente
+dict_final = OrderedDict(sorted(dict_coords.items()))
+
+# Preparação do gráfico
 
 plt.figure(figsize=(9,7))
 plt.title('Distância X Cota')
@@ -28,47 +85,27 @@ plt.xticks(np.arange(0, 1100, step=100))
 plt.ylabel('Cota [m]')
 plt.yticks(np.arange(0, 900, step=50))
 
-plt.plot(distancia, cota)
+# Plotagem do levantamento topográfico (background)
+plt.plot(distancia, cota, marker='o', color='blue', label='Levantamento Topográfico')
+plt.legend(loc='lower right')
 
-# Entrada
+# Plotagem dos dados calculados
+x = list(dict_final.keys())
+y = list(dict_final.values())
 
-print('Insira a distância da medição:')
+plt.plot(x, y, linestyle='none', marker='o', color='red', label='Cotas Interpoladas')
+plt.legend(loc='lower right')
 
-x = float(input())
+#for key, value in dict_final.items():
+#    plt.annotate(f'({key}, {round(value, 2)})', xy=(key, value), xytext=(7, 7), textcoords='offset points', xycoords='data')
 
-print(f'x =', x)
+# Exportação dos dados
 
-#for i in len(dados_entrada):
-    
-if 0 < x < 100:
-    y = 3.82 * x + 382
-    print(y)
-elif 101 < x < 200:
-    y = 0.3 * x + 736
-    print(y)
-elif 201 < x < 300:
-    y = -6.09 * x + 2014
-    print(y)
-elif 301 < x < 400:
-    y = 3.03 * x - 722
-    print(y)
-elif 401 < x < 500:
-    y = -0.44 * x + 666
-    print(y)
-elif 501 < x < 600:
-    y = 2.01 * x - 559
-    print(y)
-elif 601 < x < 700:
-    y = 0.63 * x + 269
-    print(y)
-elif 701 < x < 800:
-    y = 0.45 * x + 395
-    print(y)
-elif 801 < x < 900:
-    y = -4.78 * x + 4579
-    print(y)
-elif 901 < x < 1000:
-    y = 2.03 * x - 1550
-    print(y)
-else:
-    print('dados fora do domínio')
+df_resultados = pd.DataFrame(dict_final.items(), columns=['Distancia', 'Cota'])
+print(df_resultados)
+
+# Caminho onde o arquivo será salvo
+
+path=str(input('Digite o caminho onde o arquivo será salvo:', ))
+
+df_resultados.to_csv(f'{path}\cotas_interpoladas.csv', index=False, header=True, float_format='%.3f')
